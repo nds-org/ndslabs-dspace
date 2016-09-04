81d2e7905e93275b01496385b1321caa0a7c238c
Craig Willis <willis8@illinois.edu>
2016-09-04 15:22:04 -0500

    Added mail server

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ POSTGRES_DB_PORT=${POSTGRES_DB_PORT:-$POSTGRES_PORT_5432_TCP_PORT}
 POSTGRES_USER=${POSTGRES_USER:-dspace}
 POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-dspace}
 POSTGRES_SCHEMA=${POSTGRES_SCHEMA:-dspace}
+MAIL_SERVER=${MAIL_SERVER:-smtp.ncsa.illinois.edu}
 
 
 DSPACE_CFG=/dspace/config/dspace.cfg
@@ -14,8 +15,11 @@ DSPACE_CFG=/dspace/config/dspace.cfg
 sed -i "s#db.url = jdbc:postgresql://localhost:5432/dspace#db.url = jdbc:postgresql://${POSTGRES_DB_HOST}:${POSTGRES_DB_PORT}/${POSTGRES_SCHEMA}#" ${DSPACE_CFG}
 sed -i "s#db.username = dspace#db.username = ${POSTGRES_USER}#" ${DSPACE_CFG}
 sed -i "s#db.password = dspace#db.password = ${POSTGRES_PASSWORD}#" ${DSPACE_CFG}
-echo "Dspace configuration changed"
+sed -i "s#smtp.example.com#${MAIL_SERVER}#" ${DSPACE_CFG}
+sed -i "s#dspace-noreply@myu.edu#${ADMIN_EMAIL}#" ${DSPACE_CFG}
+sed -i "s#dspace-help@myu.edu#${ADMIN_EMAIL}#" ${DSPACE_CFG}
 
+echo "Dspace configuration changed"
 
 echo "Connecting to Postgres on $POSTGRES_DB_HOST $POSTGRES_DB_PORT"
 i=0
@@ -26,8 +30,8 @@ while true; do
     else
        if [ "$i" -lt "$TIMEOUT" ]; then 
          echo Waiting for postgres;
-		 i=$((i+5))
-	     sleep 5
+		 i=$((i+10))
+	     sleep 10 
        else 
          echo Required service Postgres not running. Have you started the required services?
          exit 1
@@ -35,6 +39,12 @@ while true; do
     fi
 done
 
-dspace create-administrator -e ${ADMIN_EMAIL} -f DSpace -l Admin -p ${ADMIN_PASSWD} -c en
+if [ -z "$ADMIN_EMAIL" ]; then
+   echo "Admin email must be specified"
+   exit 1
+else
+   echo "Creating admin user $ADMIN_EMAIL $ADMIN_PASSWD"
+   dspace create-administrator -e ${ADMIN_EMAIL} -f DSpace -l Admin -p ${ADMIN_PASSWD} -c en
+fi  
 
 exec catalina.sh run
